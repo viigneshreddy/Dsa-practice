c4cf012162a50ff6657357b03287661c31023aa8
Sriraksha Bandaru <viigneshreddy@gmail.com>
2025-03-21 16:01:34 -0500

    Add leetcode files

---
+++ leetcode/arrays/Remove Duplicates from sorted array.py
@@ -0,0 +1,16 @@
+# Leetcode
+# Problem name:Remove Duplicates from sorted array
+# Link: https://leetcode.com/problems/Remove Duplicates from sorted array
+#Difficulty:Easy
+
+class·Solution:
+    def·removeDuplicates(self,·nums:·List
+        j·=·1
+        for·i·in·range(1,·len(nums)):
+            if·nums[i]·!=·nums[i·-·1]:
+                nums[j]·=·nums[i]
+                         j·+=·1
+        return·j
+
+
+
---
+++ leetcode/arrays/Twosum.py
@@ -0,0 +1,15 @@
+# Leetcode
+# Problem name:Two Sum
+# Link: https://www.hackerrank.com/challenges/arrays-ds
+#Difficulty:Easy
+
+class Solution:
+def twoSum(self, nums: List[int], target: int) -› List[int]:
+    for 1 in range(0, len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[j] == target - nums[i]:
+                return{i,j]
+
+
+
+
