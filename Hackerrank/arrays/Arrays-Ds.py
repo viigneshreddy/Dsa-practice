4947b671c13ec7dde6d66af6e31d953d4866cab3
Sriraksha Bandaru <viigneshreddy@gmail.com>
2025-03-21 15:24:04 -0500

    Initial commit with hackerrank solutions

---
+++ Hackerrank/arrays/Arrays-Ds.py
@@ -0,0 +1,15 @@
+# Hackerrank
+# Problem name:Arrays:Ds
+# Link: https://www.hackerrank.com/challenges/arrays-ds
+#Difficulty:Easy
+
+def reverseArray(a):
+    # Write your code here
+    b =  []
+    c = -1
+    for i in range(len(a)):
+        if c < (-i):
+            b.append(a[c])
+            c = c -1
+    
+    return b
